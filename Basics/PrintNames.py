6afe50e288dae8ada5f9ba844c9f4204e8fdfe1c
swathi.nagubandi <lswathi1002@gmail.com>
2018-11-02 11:01:26 -0400

    commit

---
+++ Basics/PrintNames.py
@@ -0,0 +1,13 @@
+name1 ="hi"
+name2 ="ALL"
+
+print(name1+" "+name2)
+
+print(name1.upper()+" "+name2)
+
+print(name1.lower()+" "+name2.lower())
+
+name3= " python team"
+
+print(name1.upper()+name3.upper())
+
